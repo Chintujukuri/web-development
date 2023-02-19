6e4616c0431124bfd8ce5d994769aea20b388a9c
Chintujukuri <nithinjukuri@gmail.com>
2023-02-19 17:18:51 +0530

    initial commit

---
+++ 1.js
@@ -0,0 +1,6 @@
+document.write("hii friends");
+function square(a){
+      return a*a;
+}
+      document.write("nemed function is "+square(5));
+      
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+console.log("hii friends");
+function square(a){
+      return a*a;
+}
+      console.log("nemed function is "+square(5));
